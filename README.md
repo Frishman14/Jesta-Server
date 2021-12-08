962742d5e148be7970e77a658e8552a72e416561
Frishman <frishman.itay@gmail.com>
2021-12-08 13:37:00 +0200

    added swagger

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ then run in cmd: "npm i" and then "npm run dev"
 the server is running. 
 
 for now you should run your own mongodb instance, in the future it will be on Atlas cloud
+
+for Watching the api (swagger) go to http:localhost:4111/api-docs
--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express"), swaggerDocument = require("./swagger.json")
 
+
+// app init
 const app = express();
 app.use(bodyParser.urlencoded({
     extended: true
 }))
 app.use(bodyParser.json());
 
+// Db init
 mongoose.connect('mongodb://localhost/Jesta', { useNewUrlParser: true});
 var db = mongoose.connection;
 if(!db)
@@ -15,7 +19,11 @@ if(!db)
 else
     console.log('db connected succesfully');
 
+// Routes
 app.use('/users', require("./Routes/userRoutes.js"));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// run app
 const PORT = process.env.PORT || 4111;
 app.listen(PORT, console.log("Server start in port: " + PORT));
 
