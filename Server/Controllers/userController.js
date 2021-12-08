da391191851bd040bd54fae25099cf06fd560996
Frishman <frishman.itay@gmail.com>
2021-12-08 14:33:44 +0200

    Add crud operations users (#4)
    * added update method to user
    
    * added swagger

--- Server/Controllers/userController.js
+++ Server/Controllers/userController.js
@@ -1,9 +1,11 @@
+const { query } = require("express");
+
 User = require("../Models/User");
 
 exports.getAllUsers = (req, res) => {
     User.get(function (err, users){
         if (err) {
-            res.json({
+            return res.json({
                 status: "error",
                 message: err,
             })
@@ -39,9 +41,45 @@ exports.create = (req, res) => {
     }).catch(err => {
         console.log(err);
         if (err.name === 'MongoServerError' && err.code === 11000) {
-            return res.status(422).json({ status: "error", message: 'Email already exist' });
+            res.status(422).json({ status: "error", message: 'Email already exist' });
         } else{
             res.json({ status: "error", message: err});
         }
+    })
+}
+
+exports.deleteOne = (req, res) => {
+    if (!req.body._id && !req.body.email)
+        return res.status(406).json({ status: "error", message: 'User to delete must contain unique value'});
+    filter = {};
+    req.body._id != undefined ? filter["_id"] = req.body._id : ""; 
+    req.body.email != undefined ? filter["email"] = req.body.email : "";
+    User.deleteOne(filter).then(deletedUser => {
+            if(deletedUser.deletedCount == 0)
+                return res.json({status: "failed", message: "User has not found"});
+            res.json({status: "success", message: "User has been deleted"});
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({status: "error", message: 'Server failed'});
+        });
+};
+
+exports.updateOne = (req, res) => {
+    if (!req.body._id && !req.body.email)
+        return res.status(406).json({ status: "error", message: 'User to update must contain Id'});
+    else if (req.body.updateAttr._id)
+        return res.status(406).json({ status: "error", message: 'Canot update _Id'});
+    filter = {};
+    req.body._id != undefined ? filter["_id"] = req.body._id : ""; 
+    req.body.email != undefined ? filter["email"] = req.body.email : "";
+
+    User.findOneAndUpdate(filter, req.body.updateAttr , {runValidators : true}, function(error, doc){
+        if(error){
+            console.log(error)
+            return res.status(500).json({status: "error", message: 'Server failed'});
+        } else if (!doc){
+            return res.json({status: "failed", message: "User has not found"});
+        }
+        res.json({status: "success", message: "User has been updated"});
     })
 }
